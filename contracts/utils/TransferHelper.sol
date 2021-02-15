741373ac48f18cdf5c4f27d86dc69ea1776a2fdf
test2451 <79050952+test2451@users.noreply.github.com>
2021-02-15 22:14:21 +0530

    Update TransferHelper.sol

--- contracts/utils/TransferHelper.sol
+++ contracts/utils/TransferHelper.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.6.0;
 
-// helper methods for interacting with BEP20 tokens and sending ETH that do not consistently return true/false
+// helper methods for interacting with OIP20 tokens and sending ETH that do not consistently return true/false
 library TransferHelper {
     function safeApprove(
         address token,
@@ -35,8 +35,8 @@ library TransferHelper {
         require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FROM_FAILED');
     }
 
-    function safeTransferBNB(address to, uint256 value) internal {
+    function safeTransferOKT(address to, uint256 value) internal {
         (bool success, ) = to.call{value: value}(new bytes(0));
-        require(success, 'TransferHelper: BNB_TRANSFER_FAILED');
+        require(success, 'TransferHelper: OKT_TRANSFER_FAILED');
     }
 }
